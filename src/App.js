8f3c7a2461501d13e35993e888a81a26cbbf892a
Nowa <kloenth@gmail.com>
2020-09-06 13:04:47 -0300

    Add Toast

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from "react";
 import Routes from "./routes";
 import "./tailwind.output.css";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+toast.configure({
+  autoClose: 2000,
+  limit: 1,
+  hideProgressBar: true,
+});
 
 function App() {
   return <Routes />;
