c14105c58c28e901568ac5d2c9fd807b896b13f8
Nowaa <kloenth@gmail.com>
2020-09-07 20:53:15 -0300

    Fix toast not triggering with empty spaces in the input field

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,39 +2,39 @@ import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { toast } from "react-toastify";
 
-export default function Home() {
+const Home = () => {
   const [value, setValue] = useState("");
   const history = useHistory();
 
   async function handleTrackNumber(e) {
     e.preventDefault();
 
-    if (!value.length) {
-      toast.warn("Informe um código", {
+    if (!value.trim().length) {
+      toast("Informe um código", {
         position: toast.POSITION.TOP_CENTER,
       });
       return;
     }
 
-    history.push("/tracker/" + value.trim());
+    history.push("/tracker/" + value);
   }
 
   return (
-    <div class="flex min-h-screen items-center justify-center">
-      <div class="w-1/2">
+    <div class="flex flex-grow justify-center">
+      <div class="mt-64 w-1/2">
         <form
-          class="bg-white  shadow-md rounded px-8 pt-6 pb-8 mb-4 "
+          class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
           onSubmit={handleTrackNumber}
         >
           <div class="mb-4">
             <label
-              class="block text-gray-700 text-xl font-bold mb-2 py-3"
+              class="block text-gray-700 text-xl font-montserrat font-semibold mb-2 py-3"
               for="trackingnumber"
             >
               Rastreie o seu objeto
             </label>
             <input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:border-indigo-400"
               id="trackingnumber"
               type="text"
               placeholder="PY3690361775BR"
@@ -45,7 +45,7 @@ export default function Home() {
 
           <div class="flex items-center justify-center">
             <button
-              class="items-center bg-blue-400 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              class="items-center text-white font-montserrat font-bold py-2 px-4 rounded bg-indigo-600 hover:bg-indigo-700 focus:outline-none"
               type="submit"
             >
               Rastrear
@@ -55,4 +55,6 @@ export default function Home() {
       </div>
     </div>
   );
-}
+};
+
+export default Home;
