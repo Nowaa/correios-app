d11fe83d54946ab6c0c571e01d6472a784641346
Nowaa <kloenth@gmail.com>
2020-09-07 20:40:50 -0300

    Add Header

---
+++ src/pages/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div class="bg-white shadow">
+      <p class="text-3xl text-indigo-600 font-hind mb-4 mt-4 sm:ml-8 md:ml-32 lg:ml-40 xl:ml-64">
+        Cadê minha encomenda?
+      </p>
+    </div>
+  );
+};
+
+export default Header;
