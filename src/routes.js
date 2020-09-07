4b0cd9281dd51b742a18221a344e358b6b0c4519
Nowaa <kloenth@gmail.com>
2020-09-07 20:55:31 -0300

    Add no match route

--- src/routes.js
+++ src/routes.js
@@ -2,15 +2,23 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Header from "./pages/Header";
 import Tracker from "./pages/Tracker";
+import Footer from "./pages/Footer";
+import NotFound from "./pages/NotFound";
 
-export default function Routes() {
-  return (
-    <BrowserRouter>
+const Routes = () => (
+  <BrowserRouter>
+    <div class="flex flex-col min-h-screen">
+      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/tracker/:number" component={Tracker} />
+        <Route component={NotFound} />
       </Switch>
-    </BrowserRouter>
-  );
-}
+      <Footer />
+    </div>
+  </BrowserRouter>
+);
+
+export default Routes;
