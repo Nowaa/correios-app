94e35ca25fcd090280fd602ef2d9ce43c750fa3c
Nowa <kloenth@gmail.com>
2020-09-20 18:32:31 -0300

    Change background and font color, alter margin

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div class="bg-white shadow">
-      <p class="text-2xl text-indigo-600 font-hind mb-3 mt-3 ml-10">
+    <div class="bg-indigo-600 shadow-lg">
+      <p class="text-2xl text-gray-100 font-esteban font-normal px-2 py-2 ml-8">
         cadê minha encomenda?
       </p>
     </div>
