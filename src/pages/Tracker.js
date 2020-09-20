229d10d30031e62a9e944cd3cb38169183a14e33
Nowa <kloenth@gmail.com>
2020-09-20 18:36:48 -0300

    Add new case to switchIcon, change container and table size

--- src/pages/Tracker.js
+++ src/pages/Tracker.js
@@ -30,6 +30,8 @@ function switchIcon(params) {
       return <FaParachuteBox size={24} style={{ fill: "#f7fafc" }} />;
     case "Fiscalização aduaneira finalizada":
       return <FaFileSignature size={24} style={{ fill: "#f7fafc" }} />;
+    case "Objeto aguardando retirada no endereço indicado":
+      return <FaHourglassHalf size={24} style={{ fill: "#f7fafc" }} />;
     case "Objeto saiu para entrega ao destinatário":
       return <FaDolly size={24} style={{ fill: "#f7fafc" }} />;
     case "Objeto entregue ao destinatário":
@@ -70,7 +72,7 @@ const Tracker = (props) => {
 
     return (
       <div className="min-h-0 flex flex-grow bg-gray-100">
-        <div className="updates-container container font-montserrat mx-auto py-8 px-2 sm:max-w-full md:w-4/5 xl:w-3/5">
+        <div className="updates-container container font-montserrat mx-auto py-10 md:mt-8 px-2 sm:max-w-full md:w-4/5 xl:w-2/4">
           <div className="content">
             <div class="overflow-hidden min-w-full rounded mt-8 mb-16">
               {!error ? (
@@ -78,10 +80,10 @@ const Tracker = (props) => {
                   <table class="table-fixed">
                     <thead>
                       <tr>
-                        <th class="px-1 py-2 w-1/8"></th>
-                        <th class="px-1 py-2 w-1/8"></th>
-                        <th class="px-4 py-2 w-2/4"></th>
-                        <th class="px-4 py-2 w-3/4"></th>
+                        <th class="w-1/8"></th>
+                        <th class="w-1/8"></th>
+                        <th class="w-2/5"></th>
+                        <th class="w-3-4"></th>
                       </tr>
                     </thead>
                     <tbody>
@@ -94,23 +96,23 @@ const Tracker = (props) => {
                               {switchIcon(update.status)}
                             </td>
                             <td class="border-transparent px-1 py-2 bg-indigo-600">
-                              <p class="font-sans font-light text-gray-200 text-base lg:text-lg">
+                              <p class="font-sans font-light text-gray-200 text-base lg:text-md">
                                 {update.data}
                               </p>
-                              <p class="font-light text-gray-400 text-sm xl:text-base">
+                              <p class="font-light text-gray-400 text-sm">
                                 {update.hora}
                               </p>
                             </td>
-                            <td class="border-transparent px-3 pb-5 pt-2 bg-purple-100">
-                              <p class="font-semibold text-gray-800 text-base md:text-md lg:text-lg xl:text-lg">
+                            <td class="border-transparent px-2 bg-purple-100">
+                              <p class="font-semibold text-gray-700 text-base md:text-md lg:text-md">
                                 {update.status}
                               </p>
                             </td>
-                            <td class="font-medium text-gray-800 bg-purple-100 border-transparent text-base lg:text-md xl:text-lg">
+                            <td class="font-medium text-gray-800 bg-purple-100 text-base px-3 py-2 border-transparent lg:text-md">
                               {update.local}
-                              <p class="font-medium text-gray-800 bg-purple-100 mb-1 mr-1 text-base lg:text-md xl:text-lg">
+                              <p class="font-medium text-gray-800 bg-purple-100 text-base lg:text-md">
                                 {update.destino}
-                                <p class="font-medium text-gray-800 bg-purple-100 mt-1 mr-1 text-base lg:text-md xl:text-lg">
+                                <p class="font-medium text-gray-800 bg-purple-100 text-base lg:text-md">
                                   {update.origem}
                                 </p>
                               </p>
