8ef0f881dd9a7fbe966b4be5ad99432946655c3e
Nowa <kloenth@gmail.com>
2020-10-05 19:56:56 -0300

    Move SetStatusIcon function to utils folder, fix imports, add loading

--- src/pages/Tracker.js
+++ src/pages/Tracker.js
@@ -1,60 +1,19 @@
 import React, { useState, useEffect } from "react";
-import Error from "../pages/Error";
 
-import {
-  FaBox,
-  FaParachuteBox,
-  FaTruckLoading,
-  FaDolly,
-  FaShippingFast,
-  FaRegSmileBeam,
-  FaUserTimes,
-  FaRegSadTear,
-  FaFileSignature,
-  FaBoxes,
-  FaHourglassHalf,
-  FaTimes,
-} from "react-icons/fa";
+import Error from "../components/Error/Error";
+import Loading from "../components/Loading/Loading";
 
 import api from "../utils/api";
-
-function switchIcon(params) {
-  switch (params) {
-    case "Objeto postado":
-      return <FaBox size={24} style={{ fill: "#f7fafc" }} />;
-    case "Objeto encaminhado":
-      return <FaShippingFast size={24} style={{ fill: "#f7fafc" }} />;
-    case "Objeto recebido na unidade de exportação no país de origem":
-      return <FaTruckLoading size={24} style={{ fill: "#f7fafc" }} />;
-    case "Objeto recebido pelos Correios do Brasil":
-      return <FaParachuteBox size={24} style={{ fill: "#f7fafc" }} />;
-    case "Fiscalização aduaneira finalizada":
-      return <FaFileSignature size={24} style={{ fill: "#f7fafc" }} />;
-    case "Objeto aguardando retirada no endereço indicado":
-      return <FaHourglassHalf size={24} style={{ fill: "#f7fafc" }} />;
-    case "Objeto saiu para entrega ao destinatário":
-      return <FaDolly size={24} style={{ fill: "#f7fafc" }} />;
-    case "Objeto entregue ao destinatário":
-      return <FaRegSmileBeam size={24} style={{ fill: "#f7fafc" }} />;
-    case "Objeto recebido na unidade de distribuição":
-      return <FaBoxes size={24} style={{ fill: "#f7fafc" }} />;
-    case "Objeto ainda não chegou à unidade":
-      return <FaHourglassHalf size={24} style={{ fill: "#f7fafc" }} />;
-    case "Destinatário ausente":
-      return <FaUserTimes size={24} style={{ fill: "#f7fafc" }} />;
-    case "Objeto roubado ou extraviado":
-      return <FaRegSadTear size={24} style={{ fill: "#f7fafc" }} />;
-    default:
-      return <FaTimes size={24} style={{ fill: "#f7fafc" }} />;
-  }
-}
+import SetStatusIcon from "../utils/SetStatusIcon";
 
 const Tracker = (props) => {
   {
     const [updates, setUpdates] = useState([]);
     const [error, setError] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
+      setIsLoading(true);
       const data = {
         trackingNumber: props.match.params.number,
       };
@@ -63,9 +22,11 @@ const Tracker = (props) => {
         .get("/" + data.trackingNumber)
         .then((res) => {
           setUpdates(res.data[0]);
+          setIsLoading(false);
         })
         .catch(() => {
           setError(true);
+          setIsLoading(false);
           console.log("Código inválido");
         });
     }, [props.match.params.number]);
@@ -75,15 +36,17 @@ const Tracker = (props) => {
         <div className="updates-container container font-montserrat mx-auto py-10 md:mt-8 px-2 sm:max-w-full md:w-4/5 xl:w-2/4">
           <div className="content">
             <div class="overflow-hidden min-w-full rounded mt-8 mb-16">
-              {!error ? (
+              {isLoading ? (
+                <Loading />
+              ) : !error ? (
                 <div>
                   <table class="table-fixed">
                     <thead>
                       <tr>
                         <th class="w-1/8"></th>
                         <th class="w-1/8"></th>
-                        <th class="w-2/5"></th>
-                        <th class="w-3-4"></th>
+                        <th class="w-3/5"></th>
+                        <th class="w-2/4"></th>
                       </tr>
                     </thead>
                     <tbody>
@@ -93,13 +56,13 @@ const Tracker = (props) => {
                         .map((update) => (
                           <tr>
                             <td class="border-transparent px-2 py-2 bg-indigo-600">
-                              {switchIcon(update.status)}
+                              {SetStatusIcon(update.status)}
                             </td>
                             <td class="border-transparent px-1 py-2 bg-indigo-600">
-                              <p class="font-sans font-light text-gray-200 text-base lg:text-md">
+                              <p class="font-sans font-light text-indigo-100 text-base lg:text-md">
                                 {update.data}
                               </p>
-                              <p class="font-light text-gray-400 text-sm">
+                              <p class="font-medium text-indigo-300 text-sm">
                                 {update.hora}
                               </p>
                             </td>
@@ -108,11 +71,11 @@ const Tracker = (props) => {
                                 {update.status}
                               </p>
                             </td>
-                            <td class="font-medium text-gray-800 bg-purple-100 text-base px-3 py-2 border-transparent lg:text-md">
+                            <td class="font-medium text-gray-900 bg-purple-100 text-base px-3 py-2 border-transparent lg:text-md">
                               {update.local}
-                              <p class="font-medium text-gray-800 bg-purple-100 text-base lg:text-md">
+                              <p class="font-medium text-gray-900 bg-purple-100 text-base lg:text-md">
                                 {update.destino}
-                                <p class="font-medium text-gray-800 bg-purple-100 text-base lg:text-md">
+                                <p class="font-medium text-gray-900 bg-purple-100 text-base lg:text-md">
                                   {update.origem}
                                 </p>
                               </p>
