a694454fb4aea70a2e4b60d9ce695eb98f6932bc
Nowa <kloenth@gmail.com>
2020-10-11 16:26:11 -0300

    Add title and change text color for better contrast

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,10 @@ import { FiCoffee } from "react-icons/fi";
 
 const Footer = () => {
   return (
-    <div class="text-indigo-100 font-montserrat flex items-center justify-center bg-indigo-600 shadow-sm pr-1 pt-2 pb-2 text-xs">
+    <div class="text-gray-100 font-montserrat flex items-center justify-center bg-indigo-600 shadow-sm pr-1 pt-2 pb-2 text-xs">
       made with
       <div class="pl-1 text-sm">
-        <FiCoffee />
+        <FiCoffee title={"coffee"} />
       </div>
       <div class="pl-1 text-xs">by Nowa</div>
     </div>
