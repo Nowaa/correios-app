fcca0a08d3095e12f614109f703fd1094eb4b8f3
Nowa <kloenth@gmail.com>
2020-10-13 11:17:26 -0300

    Update URL

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001/tracking'
+    baseURL: 'https://rastreiocorreios-api.herokuapp.com/tracking'
 })
 
 export default api;
